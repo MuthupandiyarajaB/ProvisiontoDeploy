ccf89ebf452b654d7a62dd9ab589d49be1281f28
Balasubramanian <Muthupandiyaraja.Balasubramanian@FISGLOBAL.COM>
2019-11-25 19:53:05 -0600

    With Arg

--- provision.sh
+++ provision.sh
@@ -3,10 +3,10 @@ sudo apt-get install sshpass
 wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
 unzip terraform_0.11.14_linux_amd64.zip
 
-export ARM_CLIENT_ID=$1
-export ARM_CLIENT_SECRET=$2
-export ARM_SUBSCRIPTION_ID=$3
-export ARM_TENANT_ID=$4
+export ARM_CLIENT_ID="f284d432-86d7-4e91-8bfb-af4845a56f73"
+export ARM_CLIENT_SECRET="f284d432-86d7-4e91-8bfb-af4845a56f73"
+export ARM_SUBSCRIPTION_ID="139c48a9-aa7e-4313-9aff-6966aa3269c9"
+export ARM_TENANT_ID="4155e1ac-1a6a-47e0-ae4f-4895e145f22c"
 terraform init
 terraform plan -out plan.out
 terraform apply -auto-approve
