0c80118d468292635d280b47b79a11449a057158
Balasubramanian <Muthupandiyaraja.Balasubramanian@FISGLOBAL.COM>
2019-11-30 13:04:29 -0600

    Terraform init

--- provision.sh
+++ provision.sh
@@ -1,7 +1,8 @@
 sudo apt-get install unzip
+https://www.terraform.io/downloads.html
 sudo apt-get install sshpass
-wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
-unzip terraform_0.11.14_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.12.7/terraform_0.12.7_linux_amd64.zip
+unzip terraform_0.12.7_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 terraform --version
 
@@ -10,18 +11,18 @@ export ARM_CLIENT_SECRET=$2
 export ARM_SUBSCRIPTION_ID=$3
 export ARM_TENANT_ID=$4
 terraform init
-terraform plan -out plan.out
-terraform apply -auto-approve plan.out
+# terraform plan -out plan.out
+# terraform apply -auto-approve plan.out
 
-export vmss_ip=$(terraform output instance_ip_addr)
-echo "host1 ansible_ssh_port=22 ansible_ssh_host=$vmss_ip" > inventory
-echo "[all:vars]" >> inventory
-echo "ansible_connection=ssh" >> inventory
-echo "ansible_user=testadmin" >> inventory
-echo "ansible_ssh_pass=Password1234!" >> inventory
+# export vmss_ip=$(terraform output instance_ip_addr)
+# echo "host1 ansible_ssh_port=22 ansible_ssh_host=$vmss_ip" > inventory
+# echo "[all:vars]" >> inventory
+# echo "ansible_connection=ssh" >> inventory
+# echo "ansible_user=testadmin" >> inventory
+# echo "ansible_ssh_pass=Password1234!" >> inventory
 
-cat inventory
-export ANSIBLE_HOST_KEY_CHECKING=False
+# cat inventory
+# export ANSIBLE_HOST_KEY_CHECKING=False
 
 
-sudo sshpass -p "Password1234!" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r ../../_aconite-in.Sonic/drop/Sonic testadmin@$vmss_ip:~/
+# sudo sshpass -p "Password1234!" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r ../../_aconite-in.Sonic/drop/Sonic testadmin@$vmss_ip:~/
