c38fc9640735efd4ec270257aaaab56ee29dd188
Balasubramanian <Muthupandiyaraja.Balasubramanian@FISGLOBAL.COM>
2019-11-30 12:03:28 -0600

    Merge

--- provision.sh
+++ provision.sh
@@ -3,13 +3,13 @@ sudo apt-get install sshpass
 wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
 unzip terraform_0.11.14_linux_amd64.zip
 
-export ARM_CLIENT_ID="f284d432-86d7-4e91-8bfb-af4845a56f73"
-export ARM_CLIENT_SECRET="f284d432-86d7-4e91-8bfb-af4845a56f73"
-export ARM_SUBSCRIPTION_ID="139c48a9-aa7e-4313-9aff-6966aa3269c9"
-export ARM_TENANT_ID="4155e1ac-1a6a-47e0-ae4f-4895e145f22c"
+export ARM_CLIENT_ID=$1
+export ARM_CLIENT_SECRET=$2
+export ARM_SUBSCRIPTION_ID=$3
+export ARM_TENANT_ID=$4
 terraform init
 terraform plan -out plan.out
-terraform apply -auto-approve
+terraform apply -auto-approve plan.out
 
 export vmss_ip=$(terraform output instance_ip_addr)
 echo "host1 ansible_ssh_port=22 ansible_ssh_host=$vmss_ip" > inventory
@@ -20,3 +20,6 @@ echo "ansible_ssh_pass=Password1234!" >> inventory
 
 cat inventory
 export ANSIBLE_HOST_KEY_CHECKING=False
+
+
+sudo sshpass -p "Password1234!" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r ../../_aconite-in.Sonic/drop/Sonic testadmin@$vmss_ip:~/
